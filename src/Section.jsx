76964fb130cb82ae4155622179bc2965a7f799c5
Simon-Sahlin <simonsahlin@somin.se>
2024-06-10 23:27:56 +0200

    Render articles dynamically

--- src/Section.jsx
+++ src/Section.jsx
@@ -1,24 +1,41 @@
 
 
 function Section({title}){
+    
+    const items = [
+        {
+            company: "Company1",
+            title: "Title1",
+            date: "Time - time",
+            place: "Place",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, delectus!"
+        },
+        {
+            company: "Company2",
+            title: "Title2",
+            date: "Time - time",
+            place: "Place",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, delectus!"
+        }
+    ]
+    
     return(
-    <section>
-    <div className="sectionHeader">
-      <h2>{title}</h2>
-    </div>
-    <div className="sectionContent">
-      <article>
-        <h3><b>Company</b> / Title</h3>
-        <p className="datePlace">Date, Place</p>
-        <p className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, delectus!</p>
-      </article>
-      <article>
-        <h3><b>Company</b> / Title</h3>
-        <p className="datePlace">Date, Place</p>
-        <p className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, delectus!</p>
-      </article>
-    </div>
-  </section>
+        <section>
+            <div className="sectionHeader">
+                <h2>{title}</h2>
+            </div>
+            <div className="sectionContent">
+                {items.map((item) => {
+                    return(
+                        <article key={item.company}>
+                            <h3><b>{item.company}</b> / {item.title}</h3>
+                            <p className="datePlace">{item.date}, {item.place}</p>
+                            <p className="description">{item.description}</p>
+                        </article>
+                    )
+                })}
+            </div>
+    </section>
     )
 }
 
