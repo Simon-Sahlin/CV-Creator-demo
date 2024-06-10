64d676cfd2ef06c2282b59103100be786458810b
Simon-Sahlin <simonsahlin@somin.se>
2024-06-10 23:10:06 +0200

    Add Edit functionality to header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 
 import Section from './Section.jsx'
 import HeaderSec from './HeaderSec.jsx'
 
 
 function App() {
+  const [editEnabled, toggleEdit] = useState(false)
+  
+  function enableEdit(){
+    toggleEdit(true);
+  }
+  function disableEdit(){
+    toggleEdit(false);
+  }
+
   return (
     <>
+      <div className="page" id="editPage">
+        <button onClick={enableEdit}>EDIT</button>
+        <button onClick={disableEdit} disabled={!editEnabled}>SAVE</button>
+      </div>
       <div className="page">
-        <HeaderSec/>
+        <HeaderSec editEnabled={editEnabled} />
 
         <Section title="poop"/>
       </div>
--- src/HeaderSec.jsx
+++ src/HeaderSec.jsx
@@ -1,13 +1,52 @@
-function HeaderSec(){
+import { useState } from 'react'
+
+function HeaderSec({editEnabled}){
+    
+    const [fullName, setFullName] = useState("Simon Sahlin");
+    const [age, setAge] = useState("20");
+    const [phoneNum, setPhoneNum] = useState("+12 34 567 89 00");
+    const [email, setEmail] = useState("Myemail@gmail.comn");
+
+    function updateFullName(e){
+        setFullName(e.target.value);
+    }
+    function updateAge(e){
+        setAge(e.target.value);
+    }
+    function updatePhoneNum(e){
+        setPhoneNum(e.target.value);
+    }
+    function updateEmail(e){
+        setEmail(e.target.value);
+    }
+
     return(
         <section>
         <div className="sectionHeader">
-          <h1>First Name</h1>
+            {!editEnabled ?
+                (          
+                    <h1>{fullName}</h1>
+                )
+                :
+                (<>
+                    <label htmlFor="fullName">Full Name</label>
+                    <input id='fullName' type="text" value={fullName} onChange={(e)=>updateFullName(e)}/>
+                </>)
+            }
         </div>
         <div className="sectionContent">
-          <p>Age: 20</p>
-          <em><p>Phone Number: +12 34 567 89 00</p></em>
-          <em><p>Email: Myemail@gmail.com</p></em>
+            {!editEnabled ? (<>
+                <p>Age: {age}</p>
+                <em><p>Phone Number: {phoneNum}</p></em>
+                <em><p>Email: {email}</p></em>
+            </>) : (<>
+                <label htmlFor="age">Age</label>
+                <input id='age' type="number" value={age} onChange={(e)=>updateAge(e)}/>
+                <label htmlFor="phoneNum">Phone Number</label>
+                <input id='phoneNum' type="tel" value={phoneNum} onChange={(e)=>updatePhoneNum(e)}/>
+                <label htmlFor="email">Email</label>
+                <input id='email' type="email" value={email} onChange={(e)=>updateEmail(e)}/>
+            </>)}
         </div>
       </section>
     )
