33577aca09d3743bc85c8590bb5146ef4ab28a1e
Simon-Sahlin <simonsahlin@somin.se>
2024-06-10 12:46:25 +0200

    Add layout and styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,36 @@
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <div className="page">
+        <section>
+          <div className="sectionHeader">
+            <h1>First Name</h1>
+          </div>
+          <div className="sectionContent">
+            <p>Age: 20</p>
+            <em><p>Phone Number: +12 34 567 89 00</p></em>
+            <em><p>Email: Myemail@gmail.com</p></em>
+          </div>
+        </section>
+
+        <section>
+          <div className="sectionHeader">
+            <h2>Experience</h2>
+          </div>
+          <div className="sectionContent">
+            <article>
+              <h3><b>Company</b> / Title</h3>
+              <p className="datePlace">Date, Place</p>
+              <p className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, delectus!</p>
+            </article>
+            <article>
+              <h3><b>Company</b> / Title</h3>
+              <p className="datePlace">Date, Place</p>
+              <p className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, delectus!</p>
+            </article>
+          </div>
+        </section>
+      </div>
     </>
   )
 }
